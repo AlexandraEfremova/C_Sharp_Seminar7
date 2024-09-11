96158669f3df0e97cff9d7dc74105696d19b374e
Efremova Aleksandra <doctor.akab@gmail.com>
2024-09-11 20:51:38 +0300

    seminar7 : Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте значение N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от 1 до N.
+
+void Main()
+{
+    int N = ReadInt("введите натуральное число: ");
+    if(N < 1)
+    {
+        System.Console.WriteLine("Вы ввели некорректное число");
+        return;
+    }
+    PrintNaturals(N);
+}
+
+void PrintNaturals(int N)
+{
+    if(N < 1) return;
+    PrintNaturals(N - 1);
+    System.Console.Write(N + " ");
+}
+
+int ReadInt(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Main();
