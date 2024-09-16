3c046eb572a1367f5e4be0016c0b933ddf779b3e
Efremova Aleksandra <doctor.akab@gmail.com>
2024-09-16 19:52:21 +0300

    seminar7 : Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// напишите программу, которая будет принимать 
+// на вход число и возвращать сумму его цифр.
+
+void Main()
+{
+    int N = ReadInt("введите число: ");
+    System.Console.WriteLine(SumDigits(N));
+}
+
+int SumDigits(int number)
+{
+    if(number < 1)
+        return 0;
+    return number % 10 + SumDigits(number / 10); 
+}
+
+
+int ReadInt(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Main();
