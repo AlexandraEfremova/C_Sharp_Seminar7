cf30f154280f1936383db3a978ef9ca27cb4b3e5
Efremova Aleksandra <doctor.akab@gmail.com>
2024-09-16 21:17:48 +0300

    seminar7 : Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,30 @@
+﻿// считайте строку с консоли, содержащую латинские 
+// буквы. Вывести на экран согласные буквы этой строки.
+
+void Main()
+{
+    System.Console.Write("Введите строку: ");
+    string? str = Console.ReadLine();
+    PrintConsonants(str);
+}
+// i - индекс, который мы используем для того, чтобы пробегаться по строке.
+void PrintConsonants(string input)
+{
+    if(input.Length < 1) return;
+    // char c = char.ToLower(input[i]);
+    // if(IsConsonant(c))
+    // либо можно модернезировать строку, не создавая новой переменной
+    if(IsConsonant(char.ToLower(input[0])))
+        System.Console.Write(input[0] + " ");
+    PrintConsonants(input [1..]);
+}
+
+// функция, которая проверит, является ли наше значение согласным
+bool IsConsonant(char c)
+{
+    // Contains - метод, который помогает проверить содержит ли данная строка тот символ,
+    // который мы отправили на проверку
+    return"qwrtpsdfghjklzxcvbnm".Contains(c);
+}
+
+Main();
